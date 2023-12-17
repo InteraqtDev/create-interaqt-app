0bdc3886ffe39c1bf18544ac810feac99d8b7695
sskyy <skyking_H@hotmail.com>
2023-12-17 17:04:00 +0800

    fix: pkg.json

--- bin/create.js
+++ bin/create.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 import {program} from "commander";
 import {execSync} from 'child_process'
 import chalk from 'chalk'
@@ -8,7 +8,7 @@ program
     .version('0.8.0')
     .argument('<foler>', 'folder to create app')
     .action(async (folder,) => {
-
+        console.log(chalk.bgCyan.black('create interaqt app'))
         if (await existsSync(folder)) {
             console.log(chalk.red(`folder ${folder} already exists`))
             return
