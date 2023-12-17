12c2ed51ac77d1f564a2d147139f24d5f2ab1ea5
sskyy <skyking_H@hotmail.com>
2023-12-17 17:15:27 +0800

    feat: auto install dashboard

--- bin/create.js
+++ bin/create.js
@@ -4,23 +4,27 @@
 import {program} from "commander";
 import {execSync} from 'child_process'
 import chalk from 'chalk'
-import { existsSync } from 'fs'
+import {existsSync} from 'fs'
 
 program
-    .version('0.8.0')
-    .argument('<folder>', 'folder to create app')
-    .action(async (folder,) => {
-        console.log(chalk.bgCyan.black('create interaqt app'))
-        if (await existsSync(folder)) {
-            console.log(chalk.red(`folder ${folder} already exists`))
-            return
-        }
-        console.log(chalk.green(`cloning template from git@github.com:InteraqtDev/interaqt-app-boilerplate.git`))
-        await execSync(`git clone git@github.com:InteraqtDev/interaqt-app-boilerplate.git ${folder}`, {stdio: 'inherit'})
-        await process.chdir(`${folder}`)
-        console.log(chalk.green('running install script'))
-        await execSync('npm install', {stdio: 'inherit'})
-        console.log(chalk.bgCyan.black('create interaqt app succeed!'))
-    });
+  .version('0.8.0')
+  .argument('<folder>', 'folder to create app')
+  .action(async (folder,) => {
+    console.log(chalk.bgCyan.black('create interaqt app'))
+    if (await existsSync(folder)) {
+      console.log(chalk.red(`folder ${folder} already exists`))
+      return
+    }
+    console.log(chalk.green(`cloning template from git@github.com:InteraqtDev/interaqt-app-boilerplate.git`))
+    await execSync(`git clone git@github.com:InteraqtDev/interaqt-app-boilerplate.git ${folder}`, {stdio: 'inherit'})
+    await process.chdir(`${folder}`)
+    console.log(chalk.green('running install script'))
+    await execSync('npm install', {stdio: 'inherit'})
+
+    await process.chdir(`dashboard`)
+    console.log(chalk.green('running dashboard install script'))
+    await execSync('npm install', {stdio: 'inherit'})
+    console.log(chalk.bgCyan.black('create interaqt app succeed!'))
+  });
 
 program.parse(process.argv)
